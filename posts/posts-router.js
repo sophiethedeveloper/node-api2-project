753b5f16b1f38daef7ac10e9d55489280cccf759
sofia Luna <Sofia21luna@gmail.com>
2020-10-27 14:26:15 -0600

    get posts by id, and get post by comment id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -19,12 +19,44 @@ router.get('/api/posts', (req, res) => {
 
 // @desc		Get a post by id
 // @route		GET /:id
-router.get('/:id')
+router.get('/api/posts/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."});
+        }
+    }) 
+    .catch(error => {
+        res.status(500).json({
+            message: error.message,
+            stack: error.stack
+        })
+    })
+})
 
 
 // @desc		Get a post by id
 // @route		GET /:id/comments
-router.get('/api/posts/:id/comments')
+router.get('/api/posts/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then(data => {
+        if (!data.length) {
+            res.status(404).json({
+                message:  "The post with the specified ID does not exist." 
+            })
+        } else {
+            res.status(200).json(data)
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: error.message,
+            stack: error.stack
+        })
+    })
+})
 
 
  // @desc		Add a new post
